f6d6dbaeffe612301f2640cad54dd4a997cb6b73
eduardobagus <baguseduardo@gmail.com>
2024-05-31 09:54:55 +0700

    first commit

---
+++ Cafe_Mercie.py
@@ -0,0 +1,31 @@
+from datetime import datetime
+
+# Rincian coffeeshop
+def rincian_coffeshop():
+    print("=" * 38)
+    print("            Coffe Mercie")
+    print("           Jl. Mangkunegara")
+    print("            083814329508")
+
+# Menu
+menu = {
+    "1": {"nama": "Americano", "harga": 20000},
+    "2": {"nama": "Cappuccino", "harga": 20000},
+    "3": {"nama": "Caffe Latte", "harga": 25000},
+    "4": {"nama": "Mochaccino", "harga": 27000},
+    "5": {"nama": "Vanilla Latte", "harga": 25000},
+    "6": {"nama": "Matchalatte", "harga": 25000},
+    "7": {"nama": "Red Velvet", "harga": 25000},
+    "8": {"nama": "Cookies", "harga": 7000},
+    "9": {"nama": "Croissant", "harga": 15000},
+    "10": {"nama": "Muffin Chocolate", "harga": 10000},
+    "11": {"nama": "Cheesecake", "harga": 10000}
+}
+
+def tampilkan_menu():
+    print("=" * 38)
+    print("   Selamat Datang di Coffe Mercie")
+    print("Daftar Menu:")
+    for i, item in menu.items():
+        print(f"{i}. {item['nama']:<22} ({item['harga']} IDR)")
+    print("=" * 38)
